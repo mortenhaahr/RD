f300e75f08127f1180fa7a6420480288d4f7750a
Mikkel Kirkegaard <fisk9889@gmail.com>
2023-03-02 15:26:11 +0100

    New input test file

--- examples/c_style_array_converter/input_file.orig.cpp
+++ examples/c_style_array_converter/input_file.orig.cpp
@@ -14,27 +14,33 @@ public:
 /*
 static std::array<int, 4> STDArrayGlobal;
 */
-static int CStyleArrayGlobal[4];
+
+//static std::array<int, 5>
+static int normal_array_static[5];
+//static std::array<const int, 5>
+static const int normal_const_array_static[5] {0, 1, 2, 3, 4};
+//static std::array<int*, 2>
+static int* pointer_array_static[2];
+//static std::array<const int*, 2>
+static const int* const_pointer_array_static[2];
+//static std::array<int* const, 1>
+static int* const non_movable_pointer_array_static[1] = {normal_array_static};
+//static std::array<const int* const, 2>
+static const int* const non_modifyable_non_movable_pointer_array_static[2] = {normal_array_static, normal_const_array_static};
 
 int main() {
 
-//    auto test = CStyleArrayGlobal;
-//    Test test;
-
-    std::array<int, 4> STDArrayInMethod;
-
-    const double* CStyleArrayInMethod[5];
-
-    int name[5][7];
-
- //   test.STDArrayInClass[0] = 0xF4;
- //   test.CStyleArrayInClass[0] = 0xF4;
-
- //   STDArrayInMethod[0] = 0xF4;
-//    CStyleArrayInMethod[0] = 0xF4;
-
-    // Matchers:
-    //  	arrayType           initListExpr
-    //int ImplicitCStyleArray[] = {0, 4, 5, 6};
+    //std::array<int, 5>
+    int normal_array[5];
+    //std::array<const int, 5>
+    const int normal_const_array[5] {0, 1, 2, 3, 4};
+    //std::array<int*, 2>
+    int* pointer_array[2];
+    //std::array<const int*, 2>
+    const int* const_pointer_array[2];
+    //std::array<int* const, 2>
+    int* const non_movable_pointer_array[1] = {normal_array};
+    //std::array<const int* const, 2>
+    const int* const non_modifyable_non_movable_pointer_array[2] = {normal_array, normal_const_array};
 
 }
